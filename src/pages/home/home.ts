e853b8eabb4ee9081649b7b496708f56a5afc28e
Sathaporn Chumwong <mivstp3912@gmail.com>
2019-03-17 14:27:37 +0700

    createnewpage

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { AboutPage } from '../about/about';
 
 @Component({
   selector: 'page-home',
@@ -10,5 +11,7 @@ export class HomePage {
   constructor(public navCtrl: NavController) {
 
   }
-
+gotoabout(){
+  this.navCtrl.push(AboutPage);
+}
 }
